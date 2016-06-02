9f782d2d9cfdf3312abfc38fa01c0ee022609db1
Jason Scott <becomingcoder78@gmail.com>
2016-06-02 18:51:33 -0400

    Completed main game loop

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,17 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 
 public class Game {
 
     public static void main(String[] args) {
 
-        JFrame window = new JFrame("First Game");
-        window.set
+        JFrame window = new JFrame("Orb Shooter");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Closes application on window close
+
+        window.setContentPane(new GamePanel());
+
+        window.pack();  // Sets window size to whatever is inside of it
+        window.setVisible(true);
     }
 
 }
