ef69fed46b7954f011c3a59e97ac6c1e6a27f2b5
Jason Scott <becomingcoder78@gmail.com>
2016-06-08 00:23:39 -0400

    Added player class and movement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,11 @@
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.*;
 
 
-public class GamePanel extends JPanel implements Runnable {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     // FIELDS
     public static int WIDTH = 400;
@@ -17,6 +19,8 @@ public class GamePanel extends JPanel implements Runnable {
     private int FPS = 30;
     private double averageFPS;
 
+    private Player player;
+
     // CONSTRUCTOR
     public GamePanel() {
         super();
@@ -26,12 +30,16 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     // FUNCTIONS
+
+
     public void addNotify() {
         super.addNotify();
         if (thread == null) {
             thread = new Thread(this);
             thread.start();
         }
+
+        addKeyListener(this);
     }
 
     public void run() {
@@ -40,13 +48,17 @@ public class GamePanel extends JPanel implements Runnable {
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         g = (Graphics2D) image.getGraphics();
 
+        player = new Player();
+
         long startTime;
         long URDTimeMillis;
         long waitTime;
-        long totalTime;
+        long totalTime = 0;
 
-        int frameCount;
+        int frameCount = 0;
         int maxFrameCount = 30;
+
+        long targetTime = 1000 / FPS;
 
         //  GAME LOOP
         while (running) {
@@ -57,19 +69,40 @@ public class GamePanel extends JPanel implements Runnable {
             gameRender();
             gameDraw();
 
+            URDTimeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime - URDTimeMillis;
+
+            try {
+                Thread.sleep(waitTime);
+            }
+            catch (Exception e) {
+
+            }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if (frameCount == maxFrameCount) {
+                averageFPS = 1000.0 / ((totalTime / frameCount) / 1000000);
+                frameCount = 0;
+                totalTime = 0;
+            }
+
         }
 
     }
 
     private void gameUpdate() {
 
+        player.update();
     }
 
     private void gameRender() {
-        g.setColor(Color.WHITE);
+        g.setColor(new Color(0, 100, 255));
         g.fillRect(0, 0, WIDTH, HEIGHT);
         g.setColor(Color.BLACK);
-        g.drawString("I'm coding a game!!", 100, 100);
+        g.drawString("FPS: " + averageFPS, 10, 10);
+
+        player.draw(g);
 
     }
 
@@ -77,6 +110,41 @@ public class GamePanel extends JPanel implements Runnable {
         Graphics g2 = this.getGraphics();
         g2.drawImage(image, 0, 0, null);
         g2.dispose();
+
+    }
+    public void keyTyped(KeyEvent key) {
+
+    }
+    public void keyPressed(KeyEvent key) {
+        int keyCode = key.getKeyCode();
+        if (keyCode == KeyEvent.VK_LEFT) {
+            player.setLeft(true);
+        }
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            player.setRight(true);
+        }
+        if (keyCode == KeyEvent.VK_UP) {
+            player.setUp(true);
+        }
+        if (keyCode == KeyEvent.VK_DOWN) {
+            player.setDown(true);
+        }
+
+    }
+    public void keyReleased(KeyEvent key) {
+        int keyCode = key.getKeyCode();
+        if (keyCode == KeyEvent.VK_LEFT) {
+            player.setLeft(false);
+        }
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            player.setRight(false);
+        }
+        if (keyCode == KeyEvent.VK_UP) {
+            player.setUp(false);
+        }
+        if (keyCode == KeyEvent.VK_DOWN) {
+            player.setDown(false);
+        }
 
     }
 }
